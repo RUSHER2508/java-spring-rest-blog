64dfc0b96880313ea1938a0947ac3a3a77f6966b
SITARAM SHETKAR <rushu26.rs@gmail.com>
2021-10-10 20:23:51 +0530

    Initial commit

--- src/main/java/com/pluralsight/blog/BlogApplication.java
+++ src/main/java/com/pluralsight/blog/BlogApplication.java
@@ -1,10 +1,17 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.DatabaseLoader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.xml.crypto.Data;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.pluralsight")
 public class BlogApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
